713118cff2c11d1f2ff61d31f79137237330be43
KajalJadhao07 <166523843+KajalJadhao07@users.noreply.github.com>
2025-09-04 19:21:18 +0530

    Delete user-side-frontend/Frontend directory

--- user-side-frontend/Frontend/src/data/documents.js
+++
@@ -1,6 +0,0 @@
-// Mock documents
-export const documents = [
-  { name: "Aadhar Card", status: "Verified" },
-  { name: "PAN Card", status: "Verified" },
-  { name: "Income Proof", status: "Verified" }
-];
